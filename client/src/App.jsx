5610f097982470fbb2ad690ab07a83db8f9a12b5
JoshBurke0 <josburk29@nuevaschool.org>
2026-05-30 21:12:07 -0700

    worked on a lot

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,168 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useReducer } from 'react'
 import './App.css'
 import axios from 'axios'
 
 export default function App(){
 
-  const canvasWidth = 5
+  const canvasWidth = 16
   const canvasSize = canvasWidth ** 2
 
-  const [pixels, setPixels] = useState(Array(canvasSize).fill('0'))
+  const [, forceUpdate] = useReducer(x => x + 1, 0);
+  const [pixels, setPixels] = useState(Array(canvasSize).fill('white'))
   const [color, setColor] = useState('black')
+  const [title, setTitle] = useState('')
+  const [drawings, setDrawings] = useState([]) 
+  // const [currentDrawing, SetCurrentDrawing] = useState('')
+  const [isEditing, setIsEditing] = useState(false)
+  const [idToEdit, setIdToEdit] = useState('')
+  const [brushType, SetBrushType] = useState('fill')
+
+  useEffect(() =>{
+    axios.get('http://localhost:5000/api/getDrawings')
+    .then((response) =>{
+      setDrawings(response.data)
+    })
+    .catch((err) => console.log(err))
+  }, []);
+
+  function handleSubmit() {
+
+    if(isEditing == true){
+      //editing
+      console.log("editing drawing")
+      
+      const editedDrawing = {
+      title: title,
+      pixels: pixels
+      };
+      axios.put(`http://localhost:5000/api/editDrawing/${idToEdit}`, {
+      'body': editedDrawing
+      })
+      .then((response) => {
+      setDrawings(response.data)
+      });
+
+      setTitle("")
+      setIsEditing(false)
+    }
+    else{
+      const newDrawing = {
+      title: title,
+      pixels: pixels
+      };
+      axios.post('http://localhost:5000/api/newDrawing/', {
+      'body': newDrawing
+      })
+      .then((response) => {
+      setDrawings(response.data)
+      });
+
+      setTitle("")
+    }
+  }
+
+  function loadDrawingForEdit(inputDrawing){
+    setPixels(inputDrawing.pixels)
+    setTitle(inputDrawing.title)
+    setIsEditing(true)
+    setIdToEdit(inputDrawing._id)
+  }
+
+  function deleteDrawing(idToDelete){
+
+    axios.delete(`http://localhost:5000/api/deleteDrawing/${idToDelete}`)
+    .then((response) => {
+      setDrawings(response.data)
+    });
+  }
+
+  function handleClear() {
+    setPixels(Array(canvasSize).fill('white'))
+  }
 
   function selectColor(color){
     return(
-      setColor(color),
-      console.log(color)
+      setColor(color)
     )
   }
 
-  function paint(pixelIndex){
-    const updatedPixels = pixels.map((pixel, index) => {
-      if(index === pixelIndex)
-        // if(pixel != 0)
-        //   return 0
-        // else
-        //   return 1
-        return color
-      else
-        return pixel
-    });
-    setPixels(updatedPixels)
+  function loadDrawing(inputPixels){
+    setPixels(inputPixels)
+    setIsEditing(false)
   }
+
+  function brush(pixelIndex){
+      const updatedPixels = pixels.map((pixel, index) => {
+        if(index === pixelIndex)
+          return color
+        else
+          return pixel
+      });
+      setPixels(updatedPixels)
+  }
+
+  function paint(pixelIndex, clickedColor, rowIndex){
+    if(brushType === 'brush'){
+      brush(pixelIndex)
+    }
+    else{
+      // fill(pixelIndex, clickedColor, rowIndex)
+      floodFill(pixelIndex, clickedColor, rowIndex)
+    }
+  }
+
+  function fill(clickedPixelIndex, clickedColor, clickedRow){
+    console.log("attempting flood fill")
+    const pixelsToFill = [...pixels]
+    let pixelsToCheck = [clickedPixelIndex]
+
+    for(let i = 0; i < 1000; i++){
+      const item = pixelsToCheck.shift()
+      console.log("item:", item)
+      if (pixelsToFill[item] === clickedColor){
+        pixelsToFill[item] = color  
+        pixelsToCheck.push(item + 1)
+        pixelsToCheck.push(item - 1)
+        pixelsToCheck.push(item + canvasWidth)
+        pixelsToCheck.push(item - canvasWidth)
+
+      }
+    }
+    setPixels(pixelsToFill)
+  }
+
+  // function fill(clickedPixelIndex, clickedColor, clickedRow){
+  //   console.log('attempting fill')
+  //   const pixelsToFill = [...pixels]
+  //   let prevPixelsToFill = [...pixels]
+  //   console.log(pixelsToFill)
+
+    
+  //   //rows below:
+
+  //   for(let a=clickedRow; a<16; a++){
+
+  //     console.log("beginning new loop")
+  //     console.log(`clicked row: ${clickedRow}`)
+
+  //   const startOfRow = a * canvasWidth
+  //   const endOfRow = canvasWidth * (a + 1)
+
+  //     for (let i=clickedPixelIndex+(a-clickedRow)*canvasWidth; i < endOfRow; i++){
+  //       console.log("a:",a)
+  //       console.log("clickedPixelIndex:", clickedPixelIndex)
+  //       console.log("i:", clickedPixelIndex +(a-clickedRow)*canvasWidth)
+  //       if(pixels[i] === clickedColor){
+  //         pixelsToFill[i] = color
+  //         console.log('attempting brush')
+  //       }
+  //       else{
+  //         break;
+  //       }
+  //     }
+      
+      //Fill pixels to the left
+  //   xx
 
   return(
     <>
@@ -47,22 +180,42 @@ export default function App(){
                 canvasWidth={canvasWidth} 
                 canvasSize={canvasSize} 
                 paint={paint}
-                color={color}/>
+                color={color}
+                fill={fill}
+                brushType={brushType}/>
               </div>
             </div>
           </div>
           <div id='titleFrame'>
             <h1>Title: </h1>
-            <input type='text' id='titleInput'></input>
-            <button className='titleButton'>Save</button>
-            <button className='titleButton'>Clear</button>
+            <TitleInput
+            setTitle={setTitle}
+            title={title}/>
+            <Button
+            // title='Save'
+            title={isEditing === false ? 'Save' : 'Update' }
+            func={handleSubmit}
+            color='Blue'/>
+            <Button
+            title='Clear'
+            func={handleClear}
+            color='red'/>
           </div>
         </div>
         <div id='saveFrame'>
+          <div id='saveFrameTitle'>
+            <h1>Load Drawings</h1>
+          </div>
+          <div id='saveList'>
+            <SavedDrawings
+            drawings={drawings}
+            loadDrawing={loadDrawing}
+            loadDrawingForEdit={loadDrawingForEdit}
+            deleteDrawing={deleteDrawing}/>
+          </div>
         </div>
       </div>
     </>
-    
   )
 }
 
@@ -76,10 +229,9 @@ export default function App(){
           {pixels.slice(i, i + canvasWidth).map((pixel, index) => (
           <button key={`pixel-${index + i}`} 
           style={{ 
-          // backgroundColor: pixel === 1 ? 'black' : 'white',
           backgroundColor: pixel === color ? color : pixel,
           width: `${100/canvasWidth}%`}}
-          onClick={() => paint(index + i)}
+          onClick={() => paint(index + i, pixel, i/canvasWidth)}
           className='pixel'>
             &nbsp;
             </button>))}
@@ -96,9 +248,53 @@ export default function App(){
     return(
       colors.map((color) => 
       <button 
-        style={{backgroundColor: color}} className='color'
+        style={{backgroundColor: color}} 
+        className='color'
+        key={color}
         onClick={()=>selectColor(color)}>
       </button>
     )
+    )
+  }
+
+  function TitleInput({ title, setTitle }){
+    return(
+      <input 
+      type='text' 
+      id='titleInput'
+      value={title}
+      onChange={(e)=>setTitle(e.target.value)}
+      ></input>
+    )
+  }
+
+  function Button({ title, func, color}){
+    return(
+      <button 
+      className='titleButton'
+      style={{backgroundColor: color}}
+      onClick={func}
+      >{title}</button>
+    )
+  }
+
+  function SavedDrawings({ drawings, loadDrawing, deleteDrawing, loadDrawingForEdit }){
+    return(
+      <>
+        {drawings.map((drawing, index) => (
+          <div key={`${drawing.title}-${index}`}>
+            <h3>{drawing.title}</h3>
+            <button
+            onClick={() => (loadDrawing(drawing.pixels))}
+            >Load</button>
+            <button
+            onClick={() => (loadDrawingForEdit(drawing))}
+            >Edit</button>
+            <button
+            onClick={() => (deleteDrawing(drawing._id))}
+            >Delete</button>
+          </div>
+        ))}
+      </>
     )
   }
--- server/server.js
+++ server/server.js
@@ -4,13 +4,62 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 
-const taskModel = require('./schema/taskSchema')
+const drawingModel = require('./schema/drawingSchema')
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 //personalized stuff goes below here vvv
+
+app.post('/api/newDrawing', async (req, res) => {
+  try{
+    const drawing = new drawingModel({
+      title: req.body.body.title,
+      pixels: req.body.body.pixels
+    });
+    await drawing.save()
+    const allDrawings = await drawingModel.find({})
+    res.json(allDrawings)
+  } catch (err){
+    res.status(500).send(err)
+  }
+});
+
+app.get('/api/getDrawings', async (req, res) => {
+  try{
+    const allDrawings = await drawingModel.find({})
+    res.json(allDrawings)
+  } catch (err){
+    res.status(500).send(err)
+  }
+})
+
+app.delete('/api/deleteDrawing/:id', async (req, res) => {
+  try{
+    const id = req.params.id //this grabs the id from the url, as it is '/:id'
+    await drawingModel.findByIdAndDelete(id);
+    const allDrawings = await drawingModel.find({})
+    res.json(allDrawings)
+    console.log('delete hit, id:', req.params.id)
+  } catch (err){
+    res.status(500).send(err)
+  }
+})
+
+app.put('/api/editDrawing/:id', async (req, res) => {
+  try{
+    const id = req.params.id
+    await drawingModel.findByIdAndUpdate(
+      id,
+      {pixels: req.body.body.pixels}
+    )
+    const allDrawings = await drawingModel.find({})
+    res.json(allDrawings)
+  } catch (err){
+    res.status(500).send(err)
+  }
+});
 
 // Database Connection
 const PORT = process.env.PORT || 5000;
